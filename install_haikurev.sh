1740a52d5c49069f1e5026f5fbebe42d48cd3f65
Santiago Lema <santiago@lema.org>
2024-02-09 06:13:08 -0300

    fixed link installer

--- install_haikurev.sh
+++ install_haikurev.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-ln -s haikurev.sh /boot/home/config/non-packaged/bin/haikurev
+link="/boot/home/config/non-packaged/bin/haikurev"
+target=$(realpath "./haikurev.sh")
+
+if [ -L "$link" ]; then
+    echo "Symbolic link already exists."
+    read -p "Do you want to overwrite it? (y/n): " overwrite
+    echo
+    if [ "$overwrite" == "y" ]; then
+        ln -sf "$target" "$link"
+        echo "Symbolic link overwritten."
+    else
+        echo "Operation cancelled."
+    fi
+else
+    ln -s "$target" "$link"
+    
+    echo "Symbolic link created."
+fi
+echo
+echo "You can now run haikudev from anywhere"
